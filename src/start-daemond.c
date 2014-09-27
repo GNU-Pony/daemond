e0df0a070299168d2b061a77451ba5daca5f20d9
Mattias Andrée <maandree@operamail.com>
2014-09-27 02:18:24 +0200

    sanitise/set environment
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/start-daemond.c
+++ src/start-daemond.c
@@ -177,6 +177,217 @@ static int create_mqueue(void)
 
 
 /**
+ * Read an entire file, ignoring interruptions
+ * 
+ * @param   pathname  The pathname of the file
+ * @param   n         The number of read characters
+ * @return            The content of the file, `NULL` on error
+ */
+static char* read_file(const char* restrict pathname, size_t* restrict n)
+{
+  char* restrict content = NULL;
+  char* restrict old = NULL;
+  size_t ptr = 0;
+  ssize_t got;
+  int fd, saved_errno;
+  
+  if (fd = open(pathname, O_RDONLY), fd < 0)
+    return NULL;
+  
+  for (*n = 0;;)
+    {
+      if (*n - ptr < 4096)
+	{
+	  content = realloc(old = content, (*n += 8 << 10) * sizeof(char));
+	  if (content == NULL)
+	    goto fail;
+	  old = NULL;
+	}
+      got = read(fd, content + ptr, *n - ptr);
+      if ((got < 0) && (errno == EINTR))
+	continue;
+      else if (got < 0)
+	goto fail;
+      else if (got == 0)
+	break;
+      else
+	ptr += (size_t)got;
+    }
+  
+  *n = ptr / sizeof(char);
+  close(fd), fd = -1;
+  return content;
+  
+ fail:
+  saved_errno = errno;
+  free(content);
+  free(old);
+  errno = saved_errno;
+  return NULL;
+}
+
+
+/**
+ * Parse the content of the environtab
+ * 
+ * @param   environtab  The content of the environtab
+ * @return              `environ` as described by `environtab`, `NULL` on error
+ */
+static char** environtab_parse(char* restrict environtab)
+{
+  size_t saved_i = 0, saved_n = 0, n;
+  char** restrict saved_env = NULL;
+  char** restrict saved_env_old = NULL;
+  char* end;
+  char* begin;
+  char* restrict var;
+  char* restrict entry;
+  char* restrict old;
+  int saved_errno;
+  
+  for (begin = environtab;; begin = end + 1)
+    {
+      if (saved_i == saved_n)
+	{
+	  saved_env_old = saved_env;
+	  saved_env = realloc(saved_env, (saved_n += 8) * sizeof(char*));
+	  if (saved_env == NULL)
+	    goto fail;
+	  saved_env_old = NULL;
+	}
+      if (end = strchr(begin, '\n'), end == NULL)
+	break;
+      if (*end = '\0', *begin == '\0')
+	continue;
+      if (strchr(begin, '='))
+	{
+	  saved_env[saved_i] = strdup(begin);
+	  if (saved_env[saved_i++] == NULL)
+	    goto fail;
+	}
+      else if ((var = getenv(begin)))
+	{
+	  n = strlen(begin) + strlen(var) + 2;
+	  if (old = strdup(begin), old == NULL)
+	    goto fail;
+	  if (entry = realloc(old, n * sizeof(char)), entry == NULL)
+	    goto fail;
+	  saved_env[saved_i++] = entry, old = NULL;
+	  n = strlen(entry);
+	  entry[n++] = '=';
+	  memcpy(entry + n, var, (strlen(var) + 1) * sizeof(char));
+	}
+    }
+  
+  return saved_env;
+  
+ fail:
+  saved_errno = errno;
+  free(old);
+  free(saved_env);
+  free(saved_env_old);
+  errno = saved_errno;
+  return NULL;
+}
+
+
+/**
+ * Clear the environment variables and fill it with new/stored values
+ * 
+ * @param   new_environ  The new environment variables with their associated values
+ * @param   n            The number of elements in `new_environ`
+ * @return               Zero on success, -1 on error
+ */
+static int set_environ(char* const* restrict new_environ, size_t n)
+{
+  size_t i;
+  
+  if (clearenv() != 0)
+    return -1;
+  
+  for (i = 0; i < n; i++)
+    {
+      char* var = new_environ[i];
+      char* val = strchr(var, '=');
+      if (val == NULL)
+	abort();
+      *val++ = '\0';
+      if (setenv(var, val, 1) < 0)
+	return -1;
+    }
+  
+  return 0;
+}
+
+
+/**
+ * Remove unrecognised environment variables,
+ * and potentially add new environment variables
+ * 
+ * @return  Zero on success, -1 on error
+ */
+static int sanitise_environment(void)
+{
+  char* restrict old = NULL;
+  char* restrict environtab = NULL;
+  int fd = -1, rc = 0, saved_errno = 0;
+  char** restrict saved_env = NULL;
+  size_t i, n, saved_n = 0;
+  
+  /* Get environment table. */
+  if (access(SYSCONFDIR "/" PKGNAME ".d/environtab", F_OK) == 0)
+    {
+      environtab = read_file(SYSCONFDIR "/" PKGNAME ".d/environtab", &n);
+      if (environtab == NULL)
+	return -1;
+    }
+  else if (errno != ENOENT)
+    return -1;
+  else
+    /* Default table. */
+    if (environtab = strdup("PATH\n"), environtab == NULL)
+      goto fail;
+    else
+      n = strlen("PATH\n");
+  
+  /* Make sure the table ends with a new line. */
+  if (environtab[n - 1] != '\n')
+    {
+      old = environtab;
+      environtab = realloc(environtab, (n + 1) * sizeof(char));
+      if (environtab == NULL)
+	goto fail;
+      environtab[n++] = '\n';
+      old = NULL;
+    }
+  
+  /* Store environment variables before clearing and find new values. */
+  if (saved_env = environtab_parse(environtab), saved_env == NULL)
+    goto fail;
+  
+  /* Clear unwanted environment variables and add new ones. */
+  if (set_environ(saved_env, saved_n) < 0)
+    goto fail;
+  
+  goto done;
+  
+ fail:
+  saved_errno = errno;
+  rc = -1;
+  if (fd >= 0)
+    close(fd);
+  free(old);
+ done:
+  free(environtab);
+  for (i = 0; i < saved_n; i++)
+    free(saved_env[i]);
+  free(saved_env);
+  errno = saved_errno;
+  return rc;
+}
+
+
+/**
  * Do some initialisation for the daemon
  * 
  * @return  The value with which `main` should return
@@ -201,6 +412,9 @@ static int initialise_daemon(void)
     }
   
   if (signal(SIGCHLD, parent_handle_signal) == SIG_ERR)
+    return 1;
+  
+  if (sanitise_environment() < 0)
     return 1;
   
   return 0;
